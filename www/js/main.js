165634aa254e6951b1b8abe373fb32805c1cb1a7
Joel Dare <joel@joeldare.com>
2016-03-16 07:19:48 -0600

    made the edit form fields ghost text work

--- www/js/main.js
+++ www/js/main.js
@@ -8,3 +8,41 @@ function showLayout(layoutName) {
     // Show the specified layout
     layoutToShow.style.display = 'block';
 }
+
+function showHelp(buttonName) {
+    var initialValue = '';
+    var thisButton = document.querySelectorAll("input[name=" + buttonName + "]")[0];
+    switch (buttonName) {
+        case 'first':
+            initialValue = 'First Name';
+            break;
+        case 'last':
+            initialValue = 'Last Name';
+            break;
+        case 'city':
+            initialValue = 'City';
+            break;
+        case 'state':
+            initialValue = 'State';
+            break;
+        case 'zip':
+            initialValue = 'Postal Code';
+            break;
+        case 'email':
+            initialValue = 'Email Address';
+            break;
+        case 'contact':
+            initialValue = 'Best Time to Contact';
+            break;
+        default:
+            alert('Error: Bad button name - ' + buttonName);
+    }
+    // Clear the initial value
+    if (thisButton.value == initialValue) {
+        thisButton.value = '';
+        thisButton.style.color = 'black';
+    } else if (thisButton.value == '') {
+        thisButton.style.color = 'silver';
+        thisButton.value = initialValue;
+    }
+}
